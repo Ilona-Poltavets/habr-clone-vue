7365aeee43e368dea765523f7acb1fbb38dc6e1e
Ilona Poltavets <20ilona.poltavets02@gmail.com>
2025-05-26 16:40:23 +0300

    responsive

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main class="flex h-screen page-404">
-    <div class="w-1/2 relative p-8">
+    <div class="relative p-8 lg:w-1/2 hidden lg:block">
       <div class="error-message">
         <h1>Page not found</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
@@ -14,7 +14,7 @@
         </button>
       </div>
     </div>
-    <div class="w-1/2 relative">
+    <div class="w-full relative lg:w-1/2">
       <div class="error-code">404</div>
       <div class="error-img">
         <img src="@/assets/img/cat.png" alt="404 Not Found" class="" />
